5d2e21709a58f599d11c4453a390450a286f31a2
MANIVARMA-KUNIGIRI <padmasrikunigiri@gmail.com>
2023-12-31 17:14:33 +0530

    Update Gesture_Controller.py

--- Gesture_Controller.py
+++ Gesture_Controller.py
@@ -595,6 +595,5 @@ class GestureController:
         GestureController.cap.release()
         cv2.destroyAllWindows()
 
-# uncomment to run directly
-# gc1 = GestureController()
-# gc1.start()
+gc1 = GestureController()
+gc1.start()
